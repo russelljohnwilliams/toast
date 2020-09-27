ee4f60ded1e4a3b12aca28fcd86bba35c1095d44
russelljohnwilliams <russellwilliamscc@gmail.com>
2020-09-28 00:25:02 +0100

    adds email functionality

--- index.js
+++ index.js
@@ -1,26 +1,54 @@
 window.onload = function(){
   getJSON();
-  callJquery()
+  callJquery();
+  pushTheButton();
+
 }
 
-var main = function() {
+function pushTheButton(){
   var btn = document.getElementById('contact-input-button');
-  btn.onclick = handleClick;
+  btn.onclick = buttonPressed;
 }
 
-var handleClick = function() {
-  var searchedItem = document.getElementById('contact-input')
-  console.log("wooohoooo")
-  // var userInput = searchedItem.value;
-  // searchArtists(userInput)
+var buttonPressed = function() {
+  var userInput = document.getElementById('contact-input');
+  var email = validateEmail(userInput.value);
+  if (email == true){
+    postEmail(userInput.value);
+    postSuccessMessageOnScreen()
+
+  }else{
+    displayIncorrectMessage();
+  };
 };
 
 
-var searchArtists = function(userInput) {
-alert("bang bang")
+function validateEmail(email){
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
 
+function displayIncorrectMessage(){
+  alert("it looks like you have made a slight mistake. Please check your email address.");
+}
 
+function postEmail(email){
+var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+var url = "";
+xmlhttp.open("POST", url);
+xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+var string = JSON.stringify({ "email": email});
+console.log("email", string);
+xmlhttp.send(string);
+}
+
+function postSuccessMessageOnScreen(){
+  var message = "thank you";
+  var form = document.getElementById("contact-form");
+  var input = document.getElementById("contact-input");
+  form.reset();
+  input.placeholder = message;
+}
 
 function getJSON() {
   var url = "https://api.npoint.io/f596e1d88dbe9923af42";
@@ -39,21 +67,21 @@ function loopJSON(json){
   var entries = Object.entries(json);
   for (i = 0; i < entries.length; i++) {
     var entry = entries[i][1];
-    var id = entries[i][0]
+    var id = entries[i][0];
     var author = entry.author;
     var date = new Date(entry.timeStamp * 1000);
     var dateValues = getDateValues(date);
-    var comment = addComments(entry, dateValues, author, id)
+    var comment = addComments(entry, dateValues, author, id);
   }
 }
 
 function addComments(entry, dateValues, author, id){
   var comment = entry.comment;
-  var parentId = entry.parentId
+  var parentId = entry.parentId;
   if(parentId == "none"){
     loadComments(author, dateValues, comment, id);
   }else{
-    loadReply(author, dateValues, comment, id, parentId)
+    loadReply(author, dateValues, comment, id, parentId);
   }
 }
 
@@ -77,18 +105,17 @@ function getDateValues(date){
   var hour = date.getUTCHours();
   var seconds
   if(date.getUTCMinutes() < 10){seconds = "0"+date.getUTCMinutes()}else{seconds = date.getUTCMinutes().toString()};
-  var dateValues = " " +day + " " + dayNum+nth+ " " + month + " " + year +" at " + hour +":"+ seconds
-  return dateValues
+  var dateValues = " " +day + " " + dayNum+nth+ " " + month + " " + year +" at " + hour +":"+ seconds;
+  return dateValues;
 }
 
 function loadComments(author, date, comment, id){
   document.getElementById("comments-section").innerHTML += "<article  class='comment' id='"+id+"'>"+"<cite class='sans-serif-type'>"+author+"</cite><time class='sans-serif-type'>"+date+"</time><section class='sans-serif-type'>"+comment+"</section><div class='comment-reply-link  sans-serif-type'>reply</div></article>";
-
 }
 
 function loadReply(author, date, comment, id, parentId){
   var element = document.getElementById(parentId);
-  element.insertAdjacentHTML("afterend", "<article class='comment-reply indent' id='"+id+"'>"+"<cite class='sans-serif-type'>"+author+"</cite><time class='sans-serif-type'>"+date+"</time><section class='sans-serif-type'>"+comment+"</section><div class='comment-reply-link  sans-serif-type'>reply</div></article>")
+  element.insertAdjacentHTML("afterend", "<article class='comment-reply indent' id='"+id+"'>"+"<cite class='sans-serif-type'>"+author+"</cite><time class='sans-serif-type'>"+date+"</time><section class='sans-serif-type'>"+comment+"</section><div class='comment-reply-link  sans-serif-type'>reply</div></article>");
 }
 
 function callJquery(){
